a788f38335b397f8dc530bb3a17b76fc476afc0b
Hualin-Peng <34964010+Hualin-Peng@users.noreply.github.com>
2020-05-20 14:07:38 +0800

    Update Tower of Hanoi.c

--- Tower of Hanoi.c
+++ Tower of Hanoi.c
@@ -7,9 +7,7 @@ void move(char a, char c){
 }
 
 void Hanoi(int n, char a, char b, char c){
-    if (n==1)
-        move(a, c);
-    else{
+    if (n>0){
         Hanoi(n-1,a, c, b);
         move(a, c);
         Hanoi(n-1, b, a, c);
